a2ca7307b937dc6f5b838af49293706acde40fea
Elizabeth Lucas Garcia <lizzielucas.g@gmail.com>
2022-09-04 14:22:58 -0500

    changes in js

--- Calculator_web/index.js
+++ Calculator_web/index.js
@@ -1,2 +1,7 @@
 const displayValAnterior = document.getElementById('val-anterior');
 const displayValActual = document.getElementById('val-actual');
+const btnNumeros = document.querySelectorAll('.numero');
+const btnOperadores = document.querySelectorAll('.operador');
+
+const Calculadora = new Calculadora();
+console.log(Calculadora.sumar(2,3))
